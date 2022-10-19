f999501faad24cf4438af5ecdadcb20e843d17dd
anna.msklv <anna.msklv@yandex.ru>
2022-10-19 19:40:15 +0300

    task1

--- Task1_NumbersFromNTo1/Program.cs
+++ Task1_NumbersFromNTo1/Program.cs
@@ -3,13 +3,6 @@
 //  N = 5. -> "5, 4, 3, 2, 1"
 //  N = 8. -> "8, 7, 6, 5, 4, 3, 2, 1"
 
- 
-Console.Write("Введите число: ");
-int number = Convert.ToInt32(Console.ReadLine());
-int count = 1;
-NaturalToLow(number, count);
-
-
 void NaturalToLow(int n, int count)
 {
     if (count > n)
@@ -22,3 +15,7 @@ void NaturalToLow(int n, int count)
         Console.Write(count + " ");
     }
 }
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+NaturalToLow(number, count);
